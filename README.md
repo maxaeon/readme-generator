2bb8d83320007c7fc93287bc79a64eeff5252577
M.A. Parks (they/them) <87254760+maxaeon@users.noreply.github.com>
2021-09-27 18:38:43 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,30 @@
+# 9.-Node.js-Challenge-Professional-README-Generator
+
+# Purpose
+Using the command line interface, this application generates a professional README.md file for projects on GitHub, allowing project creators to devote more time to their projects. 
+
+This application specifically prompts for information about the application repository through a series of questions and generates a README.md with the Title, Description, Table of Contents, Installation instructions, Usage, License, Contributing, Tests and Questions sections.
+
+In the future, this project may be improved for editability.
+
+# Built With
+* JavaScript
+* Node.js
+
+# Installation
+1. Clone the Git repository. Navigate to the folder and run the following commands in your command line to install the npm package dependencies.
+2. "npm init"
+3. "npm install inquirer"
+4. Invoke the application with "node index.js"
+
+# Usage
+Answer prompts in command line to generate README.md.
+
+# Repo Website
+https://maxaeon.github.io/readme-generator
+
+# Questions
+For additional questions and information, please go to github.com/maxaeon/
+or reach out via email at maparks@ucdavis.edu.
+
+
---
+++ utils/generateMarkdown.js
@@ -0,0 +1,74 @@
+// // TODO: Create a function that returns a license badge based on which license is passed in
+// // If there is no license, return an empty string
+// const renderLicenseBadge = (license) => {
+//   if (license) {
+//   }
+// };
+
+// // TODO: Create a function that returns the license link
+// // If there is no license, return an empty string
+// function renderLicenseLink(license) {}
+
+// // TODO: Create a function that returns the license section of README
+// // If there is no license, return an empty string
+// function renderLicenseSection(license) {}
+
+// TODO: Create a function to generate markdown for README
+
+function generateMarkdown(data) {
+let output = `
+![License Badge](https://img.shields.io/badge/License-${data.license}-green.svg)
+
+#### GitHub Username: ${data.github}
+
+#### GitHub Email Address: ${data.email}
+
+
+# ${data.title}
+
+## Description
+${data.description}
+
+## Table of Contents
+* [Languages](#languages)
+* [Installation](#installation)
+* [Usage](#usage)
+* [Contributions](#contributions)
+* [Website](#website)
+* [Testing](#testing)
+* [Questions](#questions)
+* [License](#license)`;
+
+// regular expression (REGEX)
+data.languages = data.languages.toString().replace(/[ ]*,[ ]*|[ ]+/g, '\n* ');
+output += `
+## Built With:
+* ${data.languages}
+
+## Installation
+- ${data.installation}
+
+## Usage
+${data.usage}
+
+## Contributors
+- ${data.contributing}
+
+## Website
+${data.website}
+
+## Tests
+- ${data.test}
+
+## Questions
+For additional questions and information, please see the creator's GitHub profile here: github.com/${data.github}/
+or reach out through email at ${data.email}.
+
+## License
+Copyright &copy;${new Date().getFullYear()} by ${data.contributing}.
+Licensed under the ${data.license} license.
+`;
+return output;
+}
+
+module.exports = generateMarkdown;
